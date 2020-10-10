6fa81ea35c7c24a532d925ee1af0ce4f28afff01
ItsVeryWindy <ItsVeryWindy@users.noreply.github.com>
2020-10-10 17:28:08 +0100

    try fix shortcut order

--- src/DockerUpgradeTool/CommandLine/CommandLineShortcutFormatter.cs
+++ src/DockerUpgradeTool/CommandLine/CommandLineShortcutFormatter.cs
@@ -6,6 +6,6 @@ namespace DockerUpgradeTool.CommandLine
 {
     public static class Formatter
     {
-        public static string FormatShortcut(PropertyInfo property) => string.Join('/', property.GetCustomAttributes<ShortcutAttribute>().SelectMany(x => x.Shortcuts).OrderByDescending(x => x, StringComparer.InvariantCultureIgnoreCase));
+        public static string FormatShortcut(PropertyInfo property) => string.Join('/', property.GetCustomAttributes<ShortcutAttribute>().SelectMany(x => x.Shortcuts).OrderByDescending(x => x.Length).ThenBy(x => x, StringComparer.InvariantCultureIgnoreCase));
     }
 }
