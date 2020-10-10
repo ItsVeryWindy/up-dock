a762b6cd2a5a19d1e771380d79b5c2c304c0031a
ItsVeryWindy <ItsVeryWindy@users.noreply.github.com>
2020-10-10 17:22:38 +0100

    add string comparer

--- src/DockerUpgradeTool/CommandLine/CommandLineShortcutFormatter.cs
+++ src/DockerUpgradeTool/CommandLine/CommandLineShortcutFormatter.cs
@@ -1,10 +1,11 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Reflection;
 
 namespace DockerUpgradeTool.CommandLine
 {
     public static class Formatter
     {
-        public static string FormatShortcut(PropertyInfo property) => string.Join('/', property.GetCustomAttributes<ShortcutAttribute>().SelectMany(x => x.Shortcuts).OrderByDescending(x => x));
+        public static string FormatShortcut(PropertyInfo property) => string.Join('/', property.GetCustomAttributes<ShortcutAttribute>().SelectMany(x => x.Shortcuts).OrderByDescending(x => x, StringComparer.InvariantCultureIgnoreCase));
     }
 }
